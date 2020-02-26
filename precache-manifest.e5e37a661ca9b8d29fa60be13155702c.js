ffa5834b6552cb0cbee31ca59c73c6947adbc3b5
future <mozzobozzo35@gmail.com>
2020-02-26 21:04:30 +0200

    Updates

---
+++ precache-manifest.e5e37a661ca9b8d29fa60be13155702c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d198c9de235f22d7838919c3d786f47d",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "391b3a4a77cd7b48a935",
+    "url": "/react-todolist/static/css/main.91b59ab1.chunk.css"
+  },
+  {
+    "revision": "e5e3824ea1dd78ba3d8f",
+    "url": "/react-todolist/static/js/2.7b15004d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-todolist/static/js/2.7b15004d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391b3a4a77cd7b48a935",
+    "url": "/react-todolist/static/js/main.fc1c7cea.chunk.js"
+  },
+  {
+    "revision": "6f64616b20c79476ee69",
+    "url": "/react-todolist/static/js/runtime-main.be4f3e60.js"
+  }
+]);
